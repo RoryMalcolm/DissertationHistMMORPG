bc88fa5bcfca88c15d000c62ab3b24ea62957f15
Rory Malcolm <RoryMckenzieMalcolm@GMail.com>
2017-02-27 15:30:57 +0000

    update, windows shellscript added

--- runscript.sh
+++ runscript.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
-xbuild RepairHist_mmo/RepairHist_mmo.csproj
-xbuild TestClientROry/TestClientRory.csproj
+if [[ "$OSTYPE" == "msys" ]]; then
+    msbuild RepairHist_mmo/RepairHist_mmo.csproj
+    msbuild TestClientROry/TestClientRory.csproj
+else
+    xbuild RepairHist_mmo/RepairHist_mmo.csproj
+    xbuild TestClientROry/TestClientRory.csproj
+fi
 ./RepairHist_mmo/bin/Debug/hist_mmorpg.exe &
 ./TestClientROry/bin/Debug/TestClientROry.exe
