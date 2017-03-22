bd150280ca90464893cea844010a7d7592e574b1
rorymalcolm <RoryMckenzieMalcolm@gmail.com>
2017-03-22 21:33:07 +0000

    gtk client

---
+++ GtkClient/GtkClient/GtkClient/ProfileTable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Gtk;
+
+public class ProfileTable
+	{
+		Table ProfileLayout;
+		Label PlayerProfile;
+		Label PlayerIDLabel;
+		Label PlayerIDOutput;
+		Label PlayerNameLabel;
+		Label PlayerNameOutput;
+		Label PlayerLocationLabel;
+		Label PlayerLocatiobOutput;
+		Label PlayerArmyLabel;
+		Label PlayerArmyOutput;
+		Label PlayerPurseLabel;
+		Label PlayerPurseOutput;
+
+		public ProfileTable (string PlayerID, string PlayerName)
+		{
+			ProfileLayout = new Table (3, 2, false);
+			PlayerProfile = new Label ("Player Profile");
+			PlayerIDLabel = new Label ("Player ID:");
+			PlayerIDOutput = new Label (PlayerID);
+			PlayerNameLabel = new Label ("Player Name:");
+			PlayerNameOutput = new Label (PlayerName);
+			Window window = new Window("Profile");
+			ProfileLayout.Attach (PlayerProfile, 0, 2, 0, 1);
+			ProfileLayout.Attach (PlayerIDLabel, 0, 1, 1, 2);
+			ProfileLayout.Attach (PlayerIDOutput, 1, 2, 1, 2);
+			ProfileLayout.Attach (PlayerNameLabel, 0, 1, 2, 3);
+			ProfileLayout.Attach (PlayerNameOutput, 1, 2, 2, 3);
+			window.Add(ProfileLayout);
+			window.ShowAll();
+		}
+	}
+
+
--- GtkClient/GtkClient/GtkClient/Program.cs
+++ GtkClient/GtkClient/GtkClient/Program.cs
@@ -1,37 +1,60 @@
 ﻿using System;
 using Gtk;
+using hist_mmorpg;
 using ClientDLL;
 
 public class GtkHelloWorld {
-	static PlayerOperationsClassLib playerOps;
+	static PlayerOperations playerOps;
+	static TextTestClient client;
+	static Button northEast;
+	static Button northWest;
+	static Button east;
+	static Button west;
+	static Button southEast;
+	static Button southWest;
+	static Button fief;
+	static Button profile;
+	static Button hire;
+	static Button siege;
+	//static PlayerOperationsClassLib playerOps;
+
+	static Label currentUserOutput;
 
 	public static void Main() {
-
+		client = new TextTestClient ();
+		client.LogInAndConnect ("helen", "potato");
+		playerOps = new PlayerOperations();
+		//playerOps = new PlayerOperationsClassLib();
 		Application.Init();
-		playerOps = new PlayerOperationsClassLib();
 		//Create the Window
-		Window myWin = new Window("My first GTK# Application! ");
-		myWin.Resize(200,200);
+		Window myWin = new Window("HistMMorpg Client");
+		myWin.Resize(1000,1000);
 
 		//Create a label and put some text in it.
-		Table tableLayout = new Table(4,2,false);
-		Button northEast = new Button("North East");
-		Button northWest = new Button("North West");
-		Button east = new Button("East");
-		Button west = new Button("West");
-		Button southEast = new Button("South East");
-		Button southWest = new Button("South West");
-
-		northEast.Clicked += NorthEastClickEvent;
+		Table tableLayout = new Table(4,4,false);
+		northEast = new Button("North East");
+		northWest = new Button("North West");
+		east = new Button("East");
+		west = new Button("West");
+		southEast = new Button("South East");
+		southWest = new Button("South West");
+		fief = new Button ("Fief");
+		profile = new Button ("Profile");
+		siege = new Button ("Siege");
+		hire = new Button ("Hire");
+		SetUpDirectionalButtonClicks ();
 		Label currentUserLabel = new Label("Current User:");
-		Label currentUserOutput = new Label("");
+		currentUserOutput = new Label("");
 		//Add the label to the form
 		tableLayout.Attach(northEast, 0,1,0,1);
 		tableLayout.Attach(northWest, 1,2,0,1);
+		tableLayout.Attach (profile, 2, 3, 0, 1);
 		tableLayout.Attach(east, 0,1,1,2);
 		tableLayout.Attach(west,1,2,1,2);
+		tableLayout.Attach (siege, 2, 3, 1, 2);
 		tableLayout.Attach(southEast, 0,1,2,3);
 		tableLayout.Attach(southWest,1,2,2,3);
+		tableLayout.Attach(hire,2,3,2,3);
 		tableLayout.Attach(currentUserLabel, 0,1,3,4);
 		tableLayout.Attach(currentUserOutput,1,2,3,4);
 		myWin.Add(tableLayout);
@@ -43,7 +66,65 @@ public class GtkHelloWorld {
 	}
 
 	public static void NorthEastClickEvent(object obj, EventArgs args){
-		object hello = playerOps.Move ("NE");
+		ProtoFief move = playerOps.Move(PlayerOperations.MoveDirections.Ne, client);
+		currentUserOutput.Text = move.fiefID;
 	}
 
+	public static void NorthWestClickEvent(object obj, EventArgs args){
+		ProtoFief move = playerOps.Move(PlayerOperations.MoveDirections.Nw, client);
+		currentUserOutput.Text = move.fiefID;
+	}
+
+	public static void EastClickEvent(object obj, EventArgs args){
+		ProtoFief move = playerOps.Move(PlayerOperations.MoveDirections.E, client);
+		currentUserOutput.Text = move.fiefID;
+	}
+
+	public static void WestClickEvent(object obj, EventArgs args){
+		ProtoFief move = playerOps.Move(PlayerOperations.MoveDirections.W, client);
+		currentUserOutput.Text = move.fiefID;
+	}
+
+	public static void SouthEastClickEvent(object obj, EventArgs args){
+		ProtoFief move = playerOps.Move(PlayerOperations.MoveDirections.Se, client);
+		currentUserOutput.Text = move.fiefID;
+	}
+
+
+	public static void SouthWestClickEvent(object obj, EventArgs args){
+		ProtoFief move = playerOps.Move(PlayerOperations.MoveDirections.Sw, client);
+		currentUserOutput.Text = move.fiefID;
+	}
+
+	public static void ProfileClickEvent(object obj, EventArgs args){
+		ProtoPlayerCharacter player = playerOps.Profile (client);
+		ProfileTable profileTable = new ProfileTable (player.playerID, player.firstName + " " + player.familyName);
+	}
+
+	public static void SiegeClickEvent(object obj, EventArgs args){
+		ProtoSiegeDisplay siege = playerOps.SiegeCurrentFief (client);
+	}
+
+	public static void HireClickEvent(object obj, EventArgs args){
+	}
+
+	public static void FiefClickEvent(object obj, EventArgs args){
+		ProtoFief fief = playerOps.FiefDetails (client);
+	}
+
+	public static void SetUpDirectionalButtonClicks(){
+		northEast.Clicked += NorthEastClickEvent;
+		northWest.Clicked += NorthWestClickEvent;
+		east.Clicked += EastClickEvent;
+		west.Clicked += WestClickEvent;
+		southEast.Clicked += SouthEastClickEvent;
+		southWest.Clicked += SouthWestClickEvent;
+	}
+
+	public static void SetUpOperationButtonClicks(){
+		profile.Clicked += ProfileClickEvent;
+		siege.Clicked += SiegeClickEvent;
+		hire.Clicked += HireClickEvent;
+		fief.Clicked += FiefClickEvent;
+	}
 }
